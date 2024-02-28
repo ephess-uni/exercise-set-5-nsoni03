14ca234f484bf5489eb2aa95ddebe1a5aefb1cb2
nsoni03 <156872537+nsoni03@users.noreply.github.com>
2024-02-29 01:50:25 +0530

    Update ex_5_0.py

--- src/ex_5_0.py
+++ src/ex_5_0.py
@@ -2,7 +2,10 @@
 
 
 def line_count(infile):
-    pass
+     with open(infile, 'r') as file:
+        lines = file.readlines()
+        num_lines = len(lines)
+        print( num_lines)
 
 
 if __name__ == "__main__":
