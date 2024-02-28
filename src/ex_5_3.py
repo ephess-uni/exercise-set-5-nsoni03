df5073e72758db29848c6fd610bac041ea9f0a8e
nsoni03 <156872537+nsoni03@users.noreply.github.com>
2024-02-29 02:03:52 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -16,4 +16,13 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
+    parser.add_argument('infile',help='provide input file path',nargs='?')
+    parser.add_argument('outfile',help='provide output file path',nargs='?')
+    args = parser.parse_args()
+    raw_data = np.loadtxt(args.infile)
+    normed = (raw_data - raw_data.mean(axis=0)) / raw_data.std(axis=0)
+    processed = normed
+    root_dir = get_repository_root()
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    np.savetxt(args.outfile, processed, fmt='%.2e')
