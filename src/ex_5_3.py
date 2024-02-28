fa7f62aced841792d203b47f6c58a1b83ac7e355
nsoni03 <156872537+nsoni03@users.noreply.github.com>
2024-02-29 02:10:59 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -8,7 +8,10 @@ This module contains an entry point that:
 import numpy as np
 from argparse import ArgumentParser
 import os
-
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
 if __name__ == "__main__":
     # Create your argument parser object here.
     # Collect the filename arguments from the command line
